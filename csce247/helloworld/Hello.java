e6ca10b95adf09c2184819956b48157dffcee8a6
talankinard <kinardtd@email.sc.edu>
2025-08-21 14:04:35 -0400

    updated assignment one

--- csce247/helloworld/Hello.java
+++
@@ -1,9 +0,0 @@
-package csce247.helloworld;
-
-public class hello 
-{
-    public static void main (String [] args)
-    {
-        System.out.println("hello world");
-    }
-}
---
+++ csce247/test/src/App.java
@@ -0,0 +1,8 @@
+package csce247.test.src;
+
+public class App{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello, World!");
+    }
+}
