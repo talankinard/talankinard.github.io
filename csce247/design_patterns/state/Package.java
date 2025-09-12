7b33eb6e0d54dd68dbaae386d9c9ecb243cccbb9
talankinard <kinardtd@email.sc.edu>
2025-09-12 13:48:48 -0400

    package class

--- csce247/design_patterns/state/Package.java
+++ csce247/design_patterns/state/Package.java
@@ -1,5 +1,92 @@
 package state;
 
-public class Package {
-    
+public class Package 
+{
+    //uml
+    private String name;
+    private int quantity;
+    private State state;
+
+
+    //all states that the package could have
+    private State orderedState;
+    private State inTransitState;
+    private State deliveredState;
+
+    public Package(String name, int quantity) //construcotr
+    {
+        this.name = name;
+        this.quantity = quantity;
+
+        //states
+        this.state =null; //default state of package
+        this.orderedState = new OrderedState(this);
+        this.inTransitState = new InTransitState(this);
+        this.deliveredState = new DeliveredState(this);
+    }
+
+    //transitions of package
+    public String order()
+    {
+        setState(orderedState);
+        return state.getStatus()+"\n"+state.getETA();
+    }
+
+      public String mail()
+    {
+        setState(inTransitState);
+        return state.getStatus()+"\n"+state.getETA();
+    }
+
+    public String recieved()
+    {
+        setState(deliveredState);
+        return state.getStatus()+"\n"+state.getETA();
+    }
+
+    //getters/setters
+    public void setState(State state)
+    {
+        this.state = state;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    //getverb method
+    public String getVerb(String singular, String plural)
+    {
+        String choice;
+
+        if(quantity==1)
+        {
+            choice=singular;
+        }
+        else
+        {
+            choice=plural;
+        }
+        return choice;
+    }
+
+    public String formatName() //helper class 4 formatting
+    {
+        if(quantity==1)
+        {
+            return name;
+        }
+        else
+        {
+            return name+"'s";
+        }
+    }
+
+
 }
