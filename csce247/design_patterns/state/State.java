d76227d75e92d53f9421a4df13f0d058cc15777b
talankinard <kinardtd@email.sc.edu>
2025-09-12 13:27:45 -0400

    state class

---
+++ csce247/design_patterns/state/State.java
@@ -0,0 +1,20 @@
+package state;
+import java.util.Random;
+
+//BASE PARENT CLASS
+
+public abstract class State 
+{
+    
+    protected Package pkg;
+    protected Random random;
+
+    public State(Package pkg)
+    {
+        this.pkg = pkg;
+        this.random = new Random();
+    }
+
+    public abstract String getStatus(); //status of package
+    public abstract String getETA();
+}
