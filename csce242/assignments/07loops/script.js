5c514451da821cb64474555da0a659b1df5b5e83
talankinard <kinardtd@email.sc.edu>
2025-09-23 19:59:37 -0400

    finsihed assignment 7

--- csce242/assignments/07loops/script.js
+++ csce242/assignments/07loops/script.js
@@ -1,9 +1,40 @@
-document.getElementById("sun-moon") = () =>
-{
-    const now = new Date()
+
+const current = new Date().getHours()
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
     
-    if(now > 1)
-    {
-        console.log("hi");
-    }
+if(current >= 18 || current < 6)
+{
+    document.body.style.backgroundColor = "black";
+    sun.classList.add("hidden");
+    moon.classList.remove("hidden");
 }
+else
+{
+    sun.classList.remove("hidden");
+    moon.classList.add("hidden");
+}
+
+document.getElementById("btn-scene").onclick = () =>
+{
+    const cloudContain = document.getElementById("clouds");
+    const treeContain  = document.getElementById("trees");
+
+    for(let i=0; i<6; i++)
+    {
+        const cloud = document.createElement("div");
+        cloud.className="cloud";
+        cloud.style.top = "300px";
+        cloud.style.left = `${295 + i * 150}px`; /* cite https://stackoverflow.com/questions/24659648/absolute-positioning-for-dynamically-created-elements*/
+        cloudContain.appendChild(cloud);
+    }
+
+    for (let i = 0; i < 6; i++) 
+    {
+        const tree = document.createElement("div");
+        tree.className = "tree";
+        tree.style.top = "500px";
+        tree.style.left   = `${310 + i * 150}px`;
+        treeContain.appendChild(tree);
+    }
+};
